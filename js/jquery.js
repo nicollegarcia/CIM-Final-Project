d85dbc77ff2d39ac99d5eeb34f9abc5e428bf037
nicollegarcia <59976526+nicollegarcia@users.noreply.github.com>
2020-05-12 10:38:25 -0400

    Initial commit

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+// 	$('#pagepiling').pagepiling();
+// });
+//
+// $('#pagepiling').pagepiling({
+//     anchors: ['homesec', 'biosec', 'photos', 'music', 'events', 'contact'],
+//     menu: '#menu'
+// });
